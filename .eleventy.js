53064d52a06c4c65fc4e3a545efe862d50942bf5
Adam Silver <adambsilver@gmail.com>
2024-03-01 07:58:04 +0000

    Upgrade - remove Gulp and update CSS

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,25 @@
 const moment = require('moment');
-const cacheBuster = require('@mightyplow/eleventy-plugin-cache-buster');
+const fs = require("fs");
 
 module.exports = eleventyConfig => {
 
-  const cacheBusterOptions = {
-    outputDirectory: 'dist'
-  };
-  eleventyConfig.addPlugin(cacheBuster(cacheBusterOptions));
+  eleventyConfig.setDataDeepMerge(true);
+
+  eleventyConfig.setBrowserSyncConfig({
+    callbacks: {
+      ready: function(err, bs) {
+
+        bs.addMiddleware("*", (req, res) => {
+          const content_404 = fs.readFileSync('dist/404.html');
+          // Add 404 http status code in request header.
+          res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
+          // Provides the 404 content without redirect.
+          res.write(content_404);
+          res.end();
+        });
+      }
+    }
+  });
 
   eleventyConfig.addFilter('permalink', str => {
     return str.replace(/\.html/g, '')
@@ -24,9 +37,9 @@ module.exports = eleventyConfig => {
   });
 
   eleventyConfig
-  .addPassthroughCopy('src/assets/fonts/')
-  // .addPassthroughCopy('src/assets/images/')
-  .addPassthroughCopy('src/manifest.json')
+    .addPassthroughCopy('src/assets/fonts/')
+    // .addPassthroughCopy('src/assets/images/')
+    .addPassthroughCopy('src/manifest.json')
 
   return {
     templateFormats: ['njk', 'md', 'html', 'png', 'ico', 'svg', 'xml', 'jpg'],
